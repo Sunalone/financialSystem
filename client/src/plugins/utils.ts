cb632f472295433221d2b771c8a3de92922a6e0a
Sunalone <81159257+Sunalone@users.noreply.github.com>
2023-12-07 14:51:17 +0800

    pref: vite插件utils的initElectron方法增加注释

--- client/src/plugins/utils.ts
+++ client/src/plugins/utils.ts
@@ -3,12 +3,12 @@ const esbuild = require("esbuild");
 const initElectron = () => {
     // 使用esbuild编译TypeScript代码为JavaScript
     esbuild.buildSync({
-        entryPoints: ["src/background.ts"],
-        outfile: "dist/background.js",
+        entryPoints: ["src/background.ts"], // 入口文件
+        outfile: "dist/background.js", // 输入目录
         bundle: true,
-        platform: "node",
-        target: "node16",
-        external: ["electron"],
+        platform: "node", // 编译的目标平台
+        target: "node16", // 编译的目标环境
+        external: ["electron"], // 外部依赖项
     });
 };
 
